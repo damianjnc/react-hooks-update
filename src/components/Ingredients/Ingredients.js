a4f71ddb3b81d4d7211da67a413d9f3638516271
damianjnc <damianjnc@gmail.com>
2019-08-09 08:38:13 +0100

    state batching and state updates

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,24 +3,12 @@ import React, { useState, useEffect, useCallback } from "react";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList.js";
+import ErrorModal from "../UI/ErrorModal";
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
-  useEffect(() => {
-    fetch("https://react-update.firebaseio.com/ingredients.json")
-      .then(res => res.json())
-      .then(data => {
-        const loadedIngredients = [];
-        for (let key in data) {
-          loadedIngredients.push({
-            id: key,
-            title: data[key].title,
-            amount: data[key].amount
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      });
-  }, []);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     console.log("render ingredients", userIngredients);
@@ -31,12 +19,16 @@ function Ingredients() {
   }, []);
 
   const addIngredientHandler = ingredient => {
-    fetch("https://react-update.firebaseio.com/ingredients.json", {
+    setIsLoading(true);
+    fetch("https://react-update.fireeio.com/ingredients.json", {
       method: "POST",
       body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" }
     })
-      .then(response => response.json())
+      .then(response => {
+        setIsLoading(false);
+        return response.json();
+      })
       .then(responseData => {
         setUserIngredients(prevState => [
           ...prevState,
@@ -45,18 +37,39 @@ function Ingredients() {
             ...ingredient
           }
         ]);
-      });
+      })
+      .catch(err => setError("Something went wrong "));
   };
 
   const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients =>
-      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-    );
+    setIsLoading(true);
+    fetch(
+      `https://react-update.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: "DELETE"
+      }
+    )
+      .then(res => {
+        setIsLoading(false);
+        setUserIngredients(prevIngredients =>
+          prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+        );
+      })
+      .catch(err => setError("Something went wrong "));
+  };
+
+  const onCloseHandler = () => {
+    setError(null);
+    setIsLoading(false);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={onCloseHandler}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,31 +6,42 @@ import "./Search.css";
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [searchTerm, setSearchTerm] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      searchTerm.length === 0 ? "" : `?orderBy="title"&equalTo="${searchTerm}"`;
+    const timer = setTimeout(() => {
+      if (searchTerm === inputRef.current.value) {
+        const query =
+          searchTerm.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${searchTerm}"`;
 
-    fetch("https://react-update.firebaseio.com/ingredients.json" + query)
-      .then(res => res.json())
-      .then(data => {
-        const loadedIngredients = [];
-        for (let key in data) {
-          loadedIngredients.push({
-            id: key,
-            title: data[key].title,
-            amount: data[key].amount
+        fetch("https://react-update.firebaseio.com/ingredients.json" + query)
+          .then(res => res.json())
+          .then(data => {
+            const loadedIngredients = [];
+            for (let key in data) {
+              loadedIngredients.push({
+                id: key,
+                title: data[key].title,
+                amount: data[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [searchTerm, onLoadIngredients]);
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer); //cleanup work
+    };
+  }, [searchTerm, onLoadIngredients, inputRef]);
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={searchTerm}
             onChange={event => setSearchTerm(event.target.value)}
